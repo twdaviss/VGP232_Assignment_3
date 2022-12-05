4d9faacb0e4eecdba5b30c44a3a3df4fffbc37fd
Taylor Daviss <twdaviss99@gmail.com>
2022-12-05 04:44:11 -0800

    UI organized, images load into list

--- VGP232_Assignment_3/MainWindow.xaml.cs
+++ VGP232_Assignment_3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static TextureAtlasLib.SpriteInfo;
 using static TextureAtlasLib.Spritesheet;
+using static VGP232_Assignment_3.SpriteList;
 
 
 namespace VGP232_Assignment_3
@@ -26,6 +28,40 @@ namespace VGP232_Assignment_3
         public MainWindow()
         {
             InitializeComponent();
+            //DataContext = new SpriteList();
+        }
+
+        private void Browse_Clicked(object sender, RoutedEventArgs e)
+        {
+            Sprites.Clear();
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.Filter = "PNG | *.png;";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            //dlg.Multiselect = true;
+
+            Output_Text.Text = dlg.;
+
+        }
+
+        private void Ouput(object sender, TextChangedEventArgs e)x  
+        {
+
+        }
+
+        private void Add_Clicked(object sender, RoutedEventArgs e)
+        {
+            Sprites.Clear();
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "PNG | *.png;";
+            dlg.Multiselect = true;
+
+            Nullable<bool> result = dlg.ShowDialog();
+            foreach (string element in dlg.FileNames)
+            {
+                Sprites.Add(new BitmapImage(new Uri(element)));
+            }
+            Image_List.ItemsSource = Sprites;
         }
     }
 }
---
+++ VGP232_Assignment_3/SpriteList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace VGP232_Assignment_3
+{
+    class SpriteList 
+    {
+        public static List<ImageSource> Sprites = new List<ImageSource>();
+        public static SpriteList instance = null;
+
+        public static SpriteList Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SpriteList();
+                }
+                return instance;
+            }
+        }
+
+        public static List<ImageSource> GetSprites
+        {
+            get { return Sprites; }
+            set { 
+            }
+        }
+
+
+    }
+}
