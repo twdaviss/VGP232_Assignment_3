f64aefa40234c3c3824f704403d5ffe8232b6acc
Taylor Daviss <twdaviss99@gmail.com>
2022-12-06 17:18:25 -0800

    Assignment 3 done. Code is a little awkward at times as I made the Spritesheet into a singleton to avoid having to instantialize but this end up being really annoying for data binding and serialization, but I made it work with a couple workarounds

--- VGP232_Assignment_3/MainWindow.xaml.cs
+++ VGP232_Assignment_3/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TextureAtlasLib;
 using static TextureAtlasLib.SpriteInfo;
 using static TextureAtlasLib.Spritesheet;
 using static VGP232_Assignment_3.SpriteList;
@@ -23,12 +24,14 @@ namespace VGP232_Assignment_3
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            //DataContext = new SpriteList();
+            DataContext = new Spritesheet();
         }
 
         private void Browse_Clicked(object sender, RoutedEventArgs e)
@@ -40,11 +43,17 @@ namespace VGP232_Assignment_3
 
             //dlg.Multiselect = true;
 
-            Output_Text.Text = dlg.;
+            //sheet.OutputFile = dlg.FileName;
+            OutputDirectory = System.IO.Path.GetDirectoryName(dlg.FileName);
+            OutputFile = System.IO.Path.GetFileName(dlg.FileName);
+
+            Output_Text.Text = OutputDirectory;
+            Filename_Text.Text = OutputFile;
+
 
         }
 
-        private void Ouput(object sender, TextChangedEventArgs e)x  
+        private void Ouput(object sender, TextChangedEventArgs e)
         {
 
         }
@@ -55,13 +64,132 @@ namespace VGP232_Assignment_3
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.Filter = "PNG | *.png;";
             dlg.Multiselect = true;
-
             Nullable<bool> result = dlg.ShowDialog();
             foreach (string element in dlg.FileNames)
             {
-                Sprites.Add(new BitmapImage(new Uri(element)));
+                InputPaths.Add(element);
+                Images.Add(new BitmapImage(new Uri(element)));
             }
-            Image_List.ItemsSource = Sprites;
+            //Image_List.ItemsSource = Images;
+        }
+
+        private void Columns_Text_Changed(object sender, TextChangedEventArgs e)
+        {
+
+            if (Columns_Text.Text != "")
+            {
+                Columns = int.Parse(Columns_Text.Text);
+            }
+        }
+
+        private void MetaBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (MetaBox.IsChecked == true)
+            {
+                IncludeMetaData = true;
+            }
+            else
+            {
+                IncludeMetaData = false;
+            }
+        }
+
+        private void Generate_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            Generate(true);
+        }
+
+        private void Save_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            SaveFile(false);
+        }
+
+        private void Open_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void Exit_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Are you sure you want to exit? If so, do you want to save changes?";
+            string caption = "Word Processor";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    if (SaveFile(true)){
+                        this.Close();
+                    }
+                    else
+                    {
+                        Exit_Button_Clicked(sender, e);
+                    }
+                    break;
+                case MessageBoxResult.No:
+                    this.Close();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+            }
+        }
+
+        private void Save_As_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            SaveFile(true);
+            Save_Button.IsEnabled = true;
+        }
+
+        private void New_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Are you sure you want to start over?\nChanges will be lost.\nIf yes, do you want to save changes first?";
+            string caption = "Word Processor";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    if (SaveFile(true)){
+                        Reset();
+                    }
+                    else
+                    {
+                        New_Button_Clicked(sender,e);
+                    }
+                    break;
+                case MessageBoxResult.No:
+                    Reset();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+            }
+        }
+
+        private void Update_Directory(object sender, RoutedEventArgs e)
+        {
+            OutputDirectory = Output_Text.Text;
+        }
+
+        private void Update_File(object sender, RoutedEventArgs e)
+        {
+            OutputFile = Filename_Text.Text;
+        }
+
+        private void Remove_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            Images.RemoveAt(Image_List.SelectedIndex);
+        }
+
+        private void Columns_Text_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(Columns_Text.Text == "")
+            {
+                Columns_Text.Text = 1.ToString();
+            }
         }
     }
 }
--- VGP232_Assignment_3/TextureAtlasLib/Spritesheet.cs
+++ VGP232_Assignment_3/TextureAtlasLib/Spritesheet.cs
@@ -7,21 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Windows.Media;
+using VGP232_Assignment_3;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace TextureAtlasLib
 {
+    public class DummyClass : Spritesheet
+    {
+        public int _dcolumns;
+        public String dOutputFile { get; set; }
+        public String dOutputDirectory { get; set; }
+        public bool _dincludeMetaData;
+        public List<string> _dimages = new List<string>();
+
+        public DummyClass() { }
+        public DummyClass(bool go)
+        {
+            _dcolumns = Columns;
+            dOutputFile = OutputFile;
+            dOutputDirectory = OutputDirectory;
+            _dincludeMetaData = IncludeMetaData;
+            foreach(string element in InputPaths)
+            {
+                _dimages.Add(element);
+            }
+        }
+    }
     public class Spritesheet
     {
-        public int Columns { get; set; }
-        public String OutputFile { get; set; }
-        public String OutputDirectory { get; set; }
-        public bool IncludeMetaData { get; set; }
-        public List<String> InputPaths { get; set; }
+        public static event PropertyChangedEventHandler StaticPropertyChanged;
+        private static int _columns = 1;
+        public static int Columns {get {return _columns; } set {_columns=value; NotifyStaticPropertyChanged();}}
+        private static String _outputFile;
+        public static String OutputFile { get { return _outputFile; } set { _outputFile = value; NotifyStaticPropertyChanged(); } }
+        private static String _outputDirectory;
+        public static String OutputDirectory { get { return _outputDirectory; } set { _outputDirectory = value; NotifyStaticPropertyChanged(); } }
+        private static bool _includeMetaData;
+        public static bool IncludeMetaData
+        {
+            get
+            {
+                return _includeMetaData;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    _includeMetaData = false;
+                }
+                else
+                {
+                    _includeMetaData = value;
+                }
+                NotifyStaticPropertyChanged();
+            }
+        }
+        public static List<String> InputPaths = new List<string>();
+        public static ObservableCollection<ImageSource> _images = new ObservableCollection<ImageSource>();
+        public static ObservableCollection<ImageSource> Images
+        {
+            get { return _images; }
+            set
+            {
+                _images = value;
+            }
+        }
+        public static Spritesheet instance = null;
+        public static Spritesheet Instance { get {if (instance == null){ instance = new Spritesheet(); } return instance;}}
+        public static bool prevSaved;
+        public static string prevSavedLocation;
 
         /// <summary>
         /// Checks if the properties are set correctly. If it is not correct, then it will throw an exception.
         /// </summary>
-        private void Validate()
+        public static void CopyDummy (DummyClass dummy)
+        {
+            Columns = dummy._dcolumns;
+            OutputFile = dummy.dOutputFile;
+            OutputDirectory = dummy.dOutputDirectory;
+            IncludeMetaData = dummy._dincludeMetaData;
+
+            foreach (string element in dummy._dimages)
+            {
+                InputPaths.Add(element);
+                Images.Add(new BitmapImage(new Uri (element)));
+            }
+
+        }
+        private static void Validate()
         {
             if (InputPaths == null || InputPaths.Count == 0)
             {
@@ -42,7 +122,6 @@ namespace TextureAtlasLib
             {
                 throw new Exception("Column size must be at least 1");
             }
-
         }
 
         /// <summary>
@@ -50,7 +129,7 @@ namespace TextureAtlasLib
         /// </summary>
         /// <param name="overwrite">enable to allow the output file to be overwritten</param>
         /// 
-        public void Generate(bool overwrite)
+        public static void Generate(bool overwrite)
         {
             Validate();
 
@@ -144,8 +223,95 @@ namespace TextureAtlasLib
                 }
 
                 sheet.Save(outputPath);
+                string messageBoxText = "Sheet Successfully Generated. Would you like to view output?";
+                string caption = "Word Processor";
+                MessageBoxButton button = MessageBoxButton.YesNo;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBoxResult result;
+                result = MessageBox.Show(messageBoxText, caption, button, icon);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        Process.Start("explorer.exe",@OutputDirectory);
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
             }
         }
+        public static bool SaveFile(bool newSave)
+        {
+            if (newSave)
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.Filter = "XML | *.xml;";
+                Nullable<bool> result = dlg.ShowDialog();
+                if (dlg.FileName != "")
+                {
+                    DummyClass dummy = new DummyClass(true);
+                    FileStream fs = new FileStream(dlg.FileName, FileMode.Create);
 
+                    XmlSerializer xs = new XmlSerializer(typeof(DummyClass));
+                    xs.Serialize(fs, dummy);
+                    fs.Close();
+                    string messageBoxText = "Save Successful";
+                    string caption = "Word Processor";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Information;
+                    MessageBox.Show(messageBoxText, caption, button, icon);
+                    return true;
+                }
+            }
+            else
+            {
+                DummyClass dummy = new DummyClass(true);
+
+                FileStream fs = new FileStream(prevSavedLocation, FileMode.Create);
+
+                XmlSerializer xs = new XmlSerializer(typeof(DummyClass));
+                xs.Serialize(fs, dummy);
+                fs.Close();
+                string messageBoxText = "Save Successful";
+                string caption = "Word Processor";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBox.Show(messageBoxText, caption, button, icon);
+                return true;
+            }
+            return false;
+        }
+        public static void LoadFile()
+        {
+            var mySerializer = new XmlSerializer(typeof(DummyClass));
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "XML | *.xml;";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (dlg.FileName != "")
+            {
+                using var myFileStream = new FileStream(dlg.FileName, FileMode.Open);
+                DummyClass dummyObject = (DummyClass)mySerializer.Deserialize(myFileStream);
+
+                CopyDummy(dummyObject);
+            }
+            NotifyStaticPropertyChanged();
+        }
+        public static void Reset()
+        {
+            Columns = 0;
+            OutputFile = "";
+            OutputDirectory = "";
+            IncludeMetaData = false;
+            Images.Clear();
+            NotifyStaticPropertyChanged();
+            string messageBoxText = "All Fields Have Been Reset";
+            string caption = "Word Processor";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBox.Show(messageBoxText, caption, button, icon);
+        }
+        private static void NotifyStaticPropertyChanged([CallerMemberName] string name = null)
+        {
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(name));
+        }
     }
 }
